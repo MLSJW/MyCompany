56083f8cdce33d14e86a30a23f5c40c8f3317fcb
maxim <maximshuntov@gmail.com>
2026-04-03 16:36:49 +0300

    Добавьте файлы проекта.

---
+++ MyCompany/Infrastructure/AppConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace MyCompany.Infrastructure
+{
+    public class AppConfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public Company Company { get; set; } = new Company();
+    }
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyPhone { get; set; }
+        public string? CompanyPhoneShort { get; set; }
+        public string? CompanyEmail { get; set; }
+    }
+    public class TinyMCE
+    {
+        public string? APIKey { get; set; }
+    }
+}
