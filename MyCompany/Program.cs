f12fe18a23ab651418c45a82d9d30fc7c74be928
maxim <maximshuntov@gmail.com>
2026-04-09 17:34:51 +0300

    front razbit

--- MyCompany/Program.cs
+++ MyCompany/Program.cs
@@ -1,12 +1,21 @@
+using MyCompany.Infrastructure;
+
 namespace MyCompany
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            WebApplication app = builder.Build();
+            //подключение json и дефолтных путей
+            IConfigurationBuilder configBuild = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
+            IConfiguration configuration = configBuild.Build();
+            AppConfig config = configuration.GetSection("Project").Get < AppConfig > ()!;
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
