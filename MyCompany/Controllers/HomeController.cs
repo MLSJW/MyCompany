da129749c9b72709988ce702fa33770312e5971e
maxim <maximshuntov@gmail.com>
2026-04-17 20:50:54 +0300

    Супер промежуточный вариант верстки

--- MyCompany/Controllers/HomeController.cs
+++ MyCompany/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyCompany.Domain;
+using MyCompany.Domain.Entities;
+using MyCompany.Infrastructure;
+using MyCompany.Models;
 
 namespace MyCompany.Controllers
 {
     public class HomeController : Controller
     {
         // GET: HomeController
-        public ActionResult Index()
+        private readonly DataManager _dataManager;
+        public HomeController (DataManager dataManager)
         {
-            return View();
+            _dataManager = dataManager;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Service> list = await _dataManager.Services.GetServicesAsync();
+            IEnumerable<ServiceDTO> listDTO = HelperDTO.TransformServices(list).Take(3).ToList();
+            return View(listDTO);
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
         public IActionResult Contacts()
         { 
             return View();
